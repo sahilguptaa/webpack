06a322a1d079e6f759a028c93521e6855313a509
Sahil Gupta <gupta.sahil7696@gmail.com>
2020-02-13 09:14:30 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/
+      },
+      {
+        //        use: ["style-loader", "css-loader"],
+        loader: ExtractTextPlugin.extract({
+          loader: "css-loader"
+        }),
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css")
+    // This means any code which css-loader
+    // finds must be included in a file called styles.css.
+  ]
+};
+module.exports = config;
